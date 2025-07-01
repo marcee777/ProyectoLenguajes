015282e8120c7ae194a7e9af0003560f963e05ba
Ana Marcela <marce92005@gmail.com>
2025-06-30 21:11:04 -0600

    Add project files.

---
+++ ProyectoLenguajes/Areas/Admin/Controllers/PlatoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoPedidosExpress.Areas.Admin.Controllers
+{
+    /**
+    * Controlador del plato
+    * @author: Melanie Arce C30634
+    * @author: Carolina Rodriguez C36640
+    * @author: Marcela Rojas C36975
+    * @version: 16/06/25
+    */
+
+    [Area("Admin")]
+    public class PlatoController : Controller
+    {
+    }
+}
